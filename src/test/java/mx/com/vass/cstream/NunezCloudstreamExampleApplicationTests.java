cc5149aaf2502f50d382490daa97d2b8b7a248af
Felix Nunez <felix.nunez@vass.com.mx>
2024-03-11 13:19:07 -0700

    Se registran funciones, procesadores y consumidor

---
+++ src/test/java/mx/com/vass/cstream/NunezCloudstreamExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.vass.cstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NunezCloudstreamExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
